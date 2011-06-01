11413b8474f7cd024949fd4097a4600b69ebb12a
Owen Stephens <darcs@owenstephens.co.uk>
2011-06-01 16:36:52 +0000

    Add Bridge commands

---
+++ Bridge.hs
@@ -0,0 +1,338 @@
+{-# LANGUAGE DeriveDataTypeable #-} module Bridge( createBridge, syncBridge, VCSType(..) ) where
+
+import Utils ( die )
+import Control.Monad ( when )
+import Control.Monad.Error ( join )
+import Control.Monad.Trans.Error
+import Control.Monad.IO.Class
+import Data.ConfigFile ( emptyCP, set, to_string, readfile, get )
+import Data.Data ( Data, Typeable )
+import Data.Either.Utils ( forceEither )
+import Darcs.Lock ( withLockCanFail )
+import HSH ( runIO, unsetenv )
+import System.Directory ( createDirectory, canonicalizePath, setCurrentDirectory, copyFile, removeFile, renameFile, doesDirectoryExist )
+import System.Exit ( exitFailure, exitSuccess )
+import System.FilePath ( (</>), takeDirectory, joinPath )
+import System.IO ( hFileSize, withFile, IOMode(ReadMode) )
+import System.Posix.Files
+import System.Posix.Types ( FileMode )
+
+import Darcs.Repository ( amNotInRepository, createRepository )
+import Darcs.Repository.Prefs ( addToPreflist )
+import Darcs.Flags ( DarcsFlag(WorkRepoDir, UseFormat2) )
+import Darcs.Utils ( withCurrentDirectory )
+
+data VCSType = BareGit
+             | Git
+             | Darcs
+             deriving (Eq, Data, Typeable)
+
+instance Show VCSType where
+    show Darcs = "darcs"
+    show _ = "git"
+
+data BridgeConfig = BridgeConfig { darcs_path :: FilePath
+                                 , git_path   :: FilePath
+                                 }
+
+otherVCS :: VCSType -> VCSType
+otherVCS Darcs = Git
+otherVCS _     = Darcs
+
+data Converter = Converter { exporter :: FilePath -> IO ()
+                           , importer :: FilePath -> IO ()
+                           , markUpdater :: FilePath -> IO ()
+                           , sourceExportMarks :: FilePath
+                           , targetExportMarks :: FilePath
+                           , sourceImportMarks :: FilePath
+                           }
+
+bridgeDirName :: String
+bridgeDirName = ".darcs_bridge"
+
+darcsExportMarksName, darcsImportMarksName :: String
+darcsExportMarksName = "darcs_export_marks"
+darcsImportMarksName = "darcs_import_marks"
+gitExportMarksName, gitImportMarksName :: String
+gitExportMarksName = "git_export_marks"
+gitImportMarksName = "git_import_marks"
+
+-- |createBridge sets up a Darcs bridge. It creates a bridge folder alongside
+-- the given repository path, and creates and syncs a clone, using the "other"
+-- VCS.
+createBridge :: FilePath -> IO ()
+createBridge repoPath = do
+    fullRepoPath <- canonicalizePath repoPath
+    repoType <- identifyRepoType fullRepoPath
+    putStrLn $ unwords $ ["Identified", show repoType, "repo at", fullRepoPath]
+    targetRepoPath <- initTargetRepo fullRepoPath repoType
+    putStrLn $ unwords ["Initialised target", show $ otherVCS repoType,
+                        "repo at", targetRepoPath]
+    let topLevelDir = takeDirectory fullRepoPath
+        config   = createConfig repoType fullRepoPath targetRepoPath
+    setCurrentDirectory topLevelDir
+    createDirectory bridgeDirName
+    putStrLn $ "Created " ++ bridgeDirName ++ " in " ++ topLevelDir
+    withCurrentDirectory bridgeDirName $ do
+        writeFile "config" $ stringifyConfig config
+        createDirectory "marks"
+        -- Create empty marks files for import/export for both repos.
+        mapM_ (\f -> writeFile ("marks" </> f) "") [darcsExportMarksName,
+            darcsImportMarksName, gitExportMarksName, gitImportMarksName]
+        putStrLn $ "Wrote new marks files."
+        let writeSetExec f hook = writeFile f hook >> setFileMode f fullPerms
+        writeSetExec "hook" $ createPreHook (topLevelDir </> bridgeDirName)
+        putStrLn $ "Wrote hook."
+        setupHooks repoType fullRepoPath targetRepoPath (topLevelDir </> bridgeDirName)
+        putStrLn $ "Wired up hook in both repos. Now syncing from " ++ show repoType
+        syncBridge "." (otherVCS repoType)
+  where
+    identifyRepoType :: FilePath -> IO VCSType
+    identifyRepoType path = do
+        let searchDirs = [(["_darcs"], Darcs),
+                          ([".git"], Git),
+                          (["branches", "objects", "refs"], BareGit)]
+        mbType <- identifyRepoType' path searchDirs
+        case mbType of
+            Nothing -> die $ "Could not determine repo-type of " ++ path
+            Just vcsType -> return vcsType
+      where
+        identifyRepoType' :: FilePath -> [([FilePath], VCSType)] -> IO (Maybe VCSType)
+        identifyRepoType' _ [] = return Nothing
+        identifyRepoType' p ((paths, vcsType):fs) = do
+            exists <- mapM (doesDirectoryExist.(p </>)) paths
+            if and exists then return (Just vcsType) else identifyRepoType' p fs
+    initTargetRepo :: FilePath -> VCSType -> IO FilePath
+    initTargetRepo fullRepoPath repoType = do
+        let newPath = fullRepoPath ++ "_" ++ (show $ otherVCS repoType)
+        initTargetRepo' repoType newPath
+        return newPath
+
+    initTargetRepo' :: VCSType -> FilePath -> IO ()
+    initTargetRepo' Darcs newPath = runIO $ "git init --bare " ++ newPath
+    initTargetRepo' _ newPath = do
+        amNotInRepository [WorkRepoDir newPath] -- create repodir
+        createRepository [UseFormat2] -- create repo
+
+    createConfig :: VCSType -> FilePath -> FilePath -> BridgeConfig
+    createConfig Darcs = BridgeConfig
+    createConfig _     = flip BridgeConfig
+
+    stringifyConfig :: BridgeConfig -> String
+    stringifyConfig conf = to_string $ forceEither $ do
+        cp <- set emptyCP "DEFAULT" "darcs_path" (darcs_path conf)
+        set cp "DEFAULT" "git_path" (git_path conf)
+
+    fullPerms :: FileMode
+    fullPerms = foldr unionFileModes nullFileMode
+        [ownerModes, groupModes, otherModes]
+
+    setupHooks :: VCSType -> FilePath -> FilePath -> FilePath -> IO ()
+    setupHooks Darcs   = setupHooks'
+    setupHooks BareGit = flip setupHooks'
+    setupHooks Git = \gPath dPath -> setupHooks' dPath (gPath </> ".git")
+
+    setupHooks' :: FilePath -> FilePath -> FilePath -> IO ()
+    setupHooks' darcsPath gitPath bridgePath = do
+        let gitHookPath = joinPath [gitPath, "hooks", "pre-receive"]
+            darcsHookPath = joinPath [darcsPath, "_darcs", "hooks", "pre-apply"]
+            -- Writes a simple shell script hook file. The hook file written
+            -- simply calls the hook script in the bridge directory, passing
+            -- the appropriate VCS type.
+            writeHookFile (path, vcsType) = do
+                let hookCall = (bridgePath </> "hook") ++ " " ++ vcsType ++ "\n"
+                writeFile path hookCall
+                setFileMode path fullPerms -- TODO: Windows?
+        -- Create Darcs hooks dir.
+        createDirectory (takeDirectory darcsHookPath)
+        -- Write out hook files.
+        mapM_ writeHookFile [(darcsHookPath, "darcs"), (gitHookPath, "git")]
+        -- Update "apply" defaults, for Darcs.
+        withCurrentDirectory darcsPath $
+            addToPreflist "defaults" "apply prehook ./_darcs/hooks/pre-apply"
+
+-- |syncBridge takes a bridge folder location and a target vcs-type, and
+-- attempts to pull in any changes from the other repo, having obtained the
+-- lock, to prevent concurrent access.
+syncBridge :: FilePath -> VCSType -> IO ()
+syncBridge bridgePath repoType = do
+    fullBridgePath <- canonicalizePath bridgePath
+    setCurrentDirectory fullBridgePath
+    gotLock <- withLockCanFail "lock" (syncBridge' fullBridgePath repoType)
+    case gotLock of
+        Left _ -> putStrLn "Cannot take bridge lock!" >> exitFailure
+        _      -> exitSuccess
+
+syncBridge' :: FilePath -> VCSType -> IO ()
+syncBridge' fullBridgePath repoType = do
+    errConfig <- runErrorT $ getConfig fullBridgePath
+    case errConfig of
+        Left _ -> die $ "Malformed/missing config file in " ++ fullBridgePath
+        Right config -> do
+            let converter = createConverter repoType config fullBridgePath
+                bridgeFile fn = fullBridgePath </> fn
+                marksFile fn = joinPath [fullBridgePath, "marks", fn]
+                exportData = bridgeFile "darcs_bridge_export"
+                oldSourceMarks = bridgeFile "old_source_marks"
+                sourceMarks = marksFile $ sourceExportMarks converter
+                oldTargetMarks = bridgeFile "old_target_marks"
+                targetMarks = marksFile $ targetExportMarks converter
+                importMarks = marksFile $ sourceImportMarks converter
+                tempImportMarks = bridgeFile "import_marks"
+
+            putStrLn $ "Copying old sourcemarks: " ++ sourceMarks
+            copyFile sourceMarks oldSourceMarks
+            putStrLn "Doing export."
+            exporter converter exportData
+            size <- withFile exportData ReadMode hFileSize
+            when (size > 0) (do
+                putStrLn "Doing import."
+                importer converter exportData
+                putStrLn $ "Copying old targetmarks: " ++ targetMarks
+                -- We need to ensure that the exporting repo knows about the
+                -- mark ids of the just-imported data. We export on the
+                -- just-imported repo, to update the marks file.
+                copyFile targetMarks oldTargetMarks
+                putStrLn "Doing mark update export."
+                markUpdater converter "/dev/null"
+                -- We diff the marks files on both sides, to discover the newly
+                -- added marks. This will allow us to manually update the
+                -- import marks file of the original exporter.
+                putStrLn "Diffing marks."
+                newSourceExportMarks <- diffExportMarks oldSourceMarks sourceMarks
+                newTargetExportMarks <- diffExportMarks oldTargetMarks targetMarks
+                -- We want the source patch ids with the target mark ids
+                let patchIDs = map ((!! 1).words) newSourceExportMarks
+                    markIDs = map ((!! 0).words) newTargetExportMarks
+                    markFudger m p = alterMark m ++ " " ++ p
+                    newEntries = zipWith markFudger markIDs patchIDs
+                putStrLn $ (show $ length newEntries) ++ " marks to append."
+                -- Prepend new entries to the marks file
+                writeFile tempImportMarks $ unlines newEntries
+                existingImportMarks <- readFile importMarks
+                appendFile tempImportMarks existingImportMarks
+                renameFile tempImportMarks importMarks
+                putStrLn "Import marks updated."
+                mapM_ removeFile [oldTargetMarks, oldSourceMarks, exportData]
+
+                putStrLn "Changes were pulled in via the bridge, update your local repo."
+                -- non-zero exit-code to signal to the VCS that action is
+                -- required by the user before allowing the push/apply.
+                exitFailure)
+            mapM_ removeFile [oldSourceMarks, exportData]
+            putStrLn "No changes to pull in via the bridge."
+            exitSuccess
+  where
+    -- Darcs and Git marks both contain a colon, but it is situated at opposite
+    -- ends of the mark. Since we are copying the marks from one repo to
+    -- another, we need to convert them.
+    alterMark :: String -> String
+    alterMark (':' : ms) = ms ++ ":"
+    alterMark ms         = ':' : init ms
+
+    diffExportMarks :: FilePath -> FilePath -> IO [String]
+    diffExportMarks old new = do
+        oldLines <- getLines old
+        newLines <- getLines new
+        return $ diffExportMarks' oldLines newLines
+
+    getLines :: FilePath -> IO [String]
+    getLines file = fmap lines $ readFile file
+
+    -- New marks are prepended to the marksfile, so take until we hit a
+    -- matching line.
+    diffExportMarks' :: [String] -> [String] -> [String]
+    diffExportMarks' []     = id
+    diffExportMarks' (x:_) = takeWhile (/= x)
+
+    getConfig bridgePath = do
+        configFile <- join $ liftIO $ readfile emptyCP (bridgePath </> "config")
+        darcsPath <- get configFile "DEFAULT" "darcs_path"
+        gitPath   <- get configFile "DEFAULT" "git_path"
+        return $ BridgeConfig darcsPath gitPath
+
+-- createConverter creates a converter that will pull changes into the repo of
+-- type targetRepoType
+createConverter :: VCSType -> BridgeConfig -> FilePath -> Converter
+createConverter targetRepoType config fullBridgePath = case targetRepoType of
+        Darcs -> Converter
+            gitExport darcsImport darcsExport gitExportMarksName darcsExportMarksName gitImportMarksName
+        _     -> Converter
+            darcsExport gitImport gitExport darcsExportMarksName gitExportMarksName darcsImportMarksName
+  where
+    makeMarkPath :: String -> FilePath
+    makeMarkPath name = joinPath [fullBridgePath, "marks", name]
+
+    darcsPath = darcs_path config
+    gitPath   = git_path config
+
+    darcsImport source = do
+        let marksPath = makeMarkPath darcsImportMarksName
+            cmd = "darcs-fastconvert import --create=no --write-marks=" ++
+                marksPath ++ " --read-marks=" ++ marksPath ++ " " ++ darcsPath
+                ++ " < " ++ source
+        runIO cmd
+
+    darcsExport target = do
+        let marksPath = makeMarkPath darcsExportMarksName
+            cmd = "darcs-fastconvert export --write-marks=" ++  marksPath
+                  ++ " --read-marks=" ++ marksPath ++ " " ++ darcsPath ++ " > "
+                  ++ target
+        runIO cmd
+
+    -- This doesn't work, as we can't get stdout/stdin "back" after the
+    -- export/import has run. :(
+    --
+    --
+    -- import Marks ( handleCmdMarks )
+    -- import Import ( fastImportIncremental )
+    -- import Export ( fastExport )
+    --
+    -- import GHC.IO.Handle ( hDuplicateTo, hDuplicate )
+    --
+    -- import Control.Exception ( finally )
+    -- import System.IO (IOMode(ReadMode, WriteMode), stdout, stdin)
+    --
+    -- darcsImport source =
+    --     darcsFCCmd source stdin ReadMode darcsImportMarksName $
+    --         fastImportIncremental darcsPath
+
+    -- darcsExport target =
+    --     darcsFCCmd target stdout WriteMode darcsExportMarksName $
+    --         fastExport darcsPath
+
+    -- -- Temporarily redirect the target I/O stream to the file 'source', so that
+    -- -- we can control the input/output location. E.g. output fastExport to a
+    -- -- file, rather than stdout.
+    -- darcsFCCmd source hTarget mode marksFile toRun = do
+    --     let marksPath = makeMarkPath marksFile
+    --     withFile source mode (\h -> do
+    --         hDuplicateTo h hTarget
+    --         handleCmdMarks marksPath marksPath toRun)
+
+    gitExport target = let marksPath = makeMarkPath gitExportMarksName in
+        withCurrentDirectory gitPath $ do
+            putStrLn $ "cwd: " ++ gitPath
+            let cmd = "git fast-export --export-marks=" ++  marksPath
+                      ++ " --import-marks=" ++ marksPath ++ " HEAD > " ++ target
+            runIO cmd
+
+    gitImport source = let marksPath = makeMarkPath gitImportMarksName in
+        withCurrentDirectory gitPath  $ do
+            putStrLn $ "cwd: " ++ gitPath
+            let cmd = "git fast-import --quiet --export-marks=" ++  marksPath
+                        ++ " --import-marks=" ++ marksPath ++ " < " ++ source
+            runIO $ unsetenv ["GIT_DIR"] cmd
+
+
+-- |createPreHook returns a string containing a shell script that takes a
+-- single argument (the target vcs type) and pulls any changes into that repo.
+createPreHook :: FilePath -> String
+createPreHook bridgeDir = unlines hook where
+  hook = [ "#!/bin/sh"
+         , "[[ $1 != \"darcs\" && $1 != \"git\" ]] && "
+           ++ "echo >&2 \"Invalid repo-type: $1\" && exit 1;\n"
+         , "if [[ $1 == d* ]]; then other=\"git\"; else other=\"darcs\"; fi"
+         , "echo \"Pulling in any changes from the $other repo...\"\n"
+         , "darcs-fastconvert sync --bridge=" ++ bridgeDir
+           ++ " --repo-type=$1\n" ]
--- main.hs
+++ main.hs
@@ -2,6 +2,7 @@
 import qualified Marks
 import Import
 import Export
+import Bridge
 import System.Console.CmdLib
 
 data Cmd = Import { repo :: String
@@ -12,6 +13,9 @@ data Cmd = Import { repo :: String
          | Export { repo :: String
                   , readMarks :: FilePath
                   , writeMarks :: FilePath }
+         | CreateBridge { inputRepo :: String }
+         | Sync   { bridge :: String
+                  , repoType :: VCSType }
          deriving (Eq, Typeable, Data)
 
 instance Attributes Cmd where
@@ -23,16 +27,24 @@ instance Attributes Cmd where
     , readMarks  %> [ Help "continue conversion, previously checkpointed by --write-marks"
                     , ArgHelp "FILE" ]
     , writeMarks %> [ Help "checkpoint conversion to continue it later"
-                    , ArgHelp "FILE" ] ]
+                    , ArgHelp "FILE" ]
+    , inputRepo  %> [ Help "existing repoistory to bridge" ]
+    , repoType  %> [ Help "conversion source repository type: git or darcs"]
+    , bridge     %> [ Help "location of .darcs_bridge folder"] ]
 
-  readFlag _ = readCommon <+< readFormat
+  readFlag _ = readCommon <+< readFormat <+< readInputType
     where readFormat "darcs-2" = Darcs2Format
           readFormat "hashed" = HashedFormat
           readFormat x = error $ "No such repository format " ++ show x
+          readInputType "git" = Git
+          readInputType "darcs" = Darcs
+          readInputType x = error $ "No such input-type " ++ show x
 
 instance RecordCommand Cmd where
   mode_summary Import {} = "Import a git-fast-export dump into darcs."
   mode_summary Export {} = "Export a darcs repository to a git-fast-import stream."
+  mode_summary CreateBridge {} = "Create a darcs bridge, importing from git or darcs."
+  mode_summary Sync   {} = "Sync an existing darcs bridge."
 
 runCmdWithMarks :: Cmd -> (Marks.Marks -> IO Marks.Marks) -> IO ()
 runCmdWithMarks c act = Marks.handleCmdMarks (readMarks c) (writeMarks c) act
@@ -45,6 +57,12 @@ handleCmd c = case c of
                _  -> die "cannot create repo, with existing marksfile."
             | otherwise -> runCmdWithMarks c $ fastImportIncremental (repo c)
   Export {} -> runCmdWithMarks c $ fastExport (repo c)
+  CreateBridge {} -> case inputRepo c of
+        [] -> die "missing input-repo argument."
+        r  -> createBridge r
+  Sync {}   -> case bridge c of
+        [] -> die "missing bridge argument."
+        b  -> syncBridge b (repoType c)
 
 main :: IO ()
 main = getArgs >>= dispatchR [] >>= handleCmd
