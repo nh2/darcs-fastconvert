ae733bae1fc280fe1c7cd1401d244a2658ef86ac
Owen Stephens <darcs@owenstephens.co.uk>
2011-07-14 13:28:33 +0000

    Use ignore-times in failing test.

--- tests/export_divergent_incremental.sh
+++ tests/export_divergent_incremental.sh
@@ -6,7 +6,7 @@ darcs init --repo R
 cd R
 echo -e '1\n2\n3' > a
 darcs add a
-darcs rec -am 'add a'
+darcs rec --ignore-times -am 'add a'
 cd ..
 
 darcs-fastconvert export --write-marks export_marks R | darcs-fastconvert import --write-marks import_marks T
@@ -14,10 +14,11 @@ darcs-fastconvert export --write-marks export_marks R | darcs-fastconvert import
 darcs get -q R S
 cd R
 echo -e '1\n2\n4' > a
-darcs rec -am 'change a R'
+darcs rec --ignore-times -am 'change a R'
+
 cd ../S
 echo -e '1\n2\n5' > a
-darcs rec -am 'change a S'
+darcs rec --ignore-times -am 'change a S'
 cd ..
 
 darcs-fastconvert export --read-marks export_marks R S | darcs-fastconvert import --create=no --read-marks import_marks T
